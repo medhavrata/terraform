e39a0c56de25a79364603f2c6c000c0af91cd11a
Medha <tyagi.25@gmail.com>
2021-11-09 22:28:15 +0000

    done some changes

--- main.tf
+++ main.tf
@@ -5,8 +5,28 @@ provider "aws"{
 resource "aws_instance" "example" {
     ami = "ami-074771aa49ab046e7"
     instance_type = "t2.micro"
+    #vpc_security_group_ids = [ "aws_security_group.instance.id" ]
+
+    # user_data = <<-EOF
+    #             #!/bin/bash
+    #             echo "Hello, World" > index.html
+    #             nohup busybox httpd -f -p 8080 &
+    #             EOF
 
     tags = {
       "Name" = "terraform-example"
     }
 }
+
+# resource "aws_security_group" "instance" {
+#   name = "terraform-example-instance"
+
+#   ingress = [ {
+#     description = "To attach with terraform-example"
+#     cidr_blocks = [ "0.0.0.0/0" ]
+#     ipv6_cidr_blocks = ["::/0"]
+#     from_port = 8080
+#     protocol = "tcp"
+#     to_port = 8080
+#   } ]
+# }
